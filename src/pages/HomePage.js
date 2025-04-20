25190aa0bea50c0feeeb825a6bf85989ae0ec88d
carolaraujo <anacarolinadearaujonunes@gmail.com>
2025-04-19 22:37:02 -0300

    #2 - initial structure for backend

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to BeautyBooker</h1>
+            <p>Schedule your beauty services here!</p>
+        </div>
+    );
+}
+
+export default HomePage;
